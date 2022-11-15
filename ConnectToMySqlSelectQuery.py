308afdfccd0defe3639d32bef52b2f21f11d6be2
MM <mario106c@gmail.com>
2022-11-15 17:36:24 +0100

    add mysql

--- ConnectToMySqlSelectQuery.py
+++ ConnectToMySqlSelectQuery.py
@@ -11,16 +11,16 @@ try:
                                          database='MyFisrtDatBase',
                                          user='root',
                                          password='')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
 
-except Error as e:
-    print("Error while connecting to MySQL", e)
+    mySql_Select_Query = """select * from City; """
+
+    cursor = connection.cursor()
+    result = cursor.execute(mySql_Select_Query)
+    record = cursor.fetchall()
+    print("Your connected to database: ", record)
+
+except mysql.connector.Error as error:
+    print("Failed to select table in MySQL: {}".format(error))
 finally:
     if connection.is_connected():
         cursor.close()
